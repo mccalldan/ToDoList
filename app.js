df50f3ef22eeb719249d60f4727ed93b58ecf23b
mccalldan <drm8266@gmail.com>
2019-09-30 21:48:20 -0400

    gitignore

--- app.js
+++ app.js
@@ -149,6 +149,6 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(process.ENV.PORT || 3000, function() {
+  console.log("Server started!");
 });
